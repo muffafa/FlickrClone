5fd5e4a4758dd8bd700911e186fdff64cdc1b4a3
muffafa <mm.savar@gmail.com>
2023-04-10 10:04:12 +0300

    init

---
+++ FlickrClone/Screens/RecentPhotos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlickrClone
+//
+//  Created by Muhammed Mustafa Savar on 10.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
